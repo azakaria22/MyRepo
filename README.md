2871a3136c8689d387c175333a46a6a9ac465aea
Aiman <zakaria.aiman@gmail.com>
2018-06-03 20:39:44 -0700

    - Added basic design and structure to page. - Added two fields, one is a short name the other is the snippet - Added capability to store the name and code inside localStorage - Added capability to display all snippets - Added capability to  search for a snippet using snippet name.

--- README.md
+++ README.md
@@ -3,6 +3,18 @@ Making a new repo
 
 Goals:
 
-- [X]  input from single input field.
-- [X] click handler to capture data from input field. 
-- [ ] helper utilities to read and write to local storage. 
+ - [X] Input form with single text input
+
+ - [X] click handler to capture data in text form field
+
+ - [X] helper utilities to read and write to local storage
+
+ - [X] Make it handle code! (use a textarea instead of a textfield)
+
+ - [X] Have two fields, one is a short name the other is the snippet
+
+ - [X] Going to store the name and code inside localStorage
+
+ - [X] Display all snippets
+
+ - [X] Search for a snippet using the name
--- app.js
+++ app.js
@@ -1,16 +1,54 @@
 $(document).ready(function() {
-	$('.setData').on('click', function(event){
-		let $textFieldValue = $('.textField').val();
-		$('.debug').text($textFieldValue);
-
-		localStorage.setItem("myFormTextData", $textFieldValue);
+	$('.setData').on('click', function(){
+		let formData = $('form').serializeArray();
+		localStorage.setItem(formData[0].value, formData[1].value);
 	});
 
-	$('.getData').on('click', function(event){
-		let retrieved = localStorage.getItem("myFormTextData");
-		$('.debug').text(retrieved);
+	$('.getData').on('click', function(event) {
+		let retrievedSnippets = $('.retrievedSnippets');
+		retrievedSnippets.empty();
+		let numberOfSnippets = localStorage.length;
+		for(let i = 0; i < numberOfSnippets; i++) {
+			let snippetName = localStorage.key(i);
+			let snippet = localStorage.getItem(snippetName);
+			let snippetContainer = $('<div></div>').addClass("snippet");
+			$('<h4></h4>').html(snippetName)
+			.appendTo(snippetContainer);
+			$('<code></code>').html(snippet)
+			.appendTo(snippetContainer);
+			snippetContainer.appendTo(retrievedSnippets);
+			event.preventDefault();
+		}
+		retrievedSnippets.toggle('slow', function() {
+			if ($('.getData').html() === "Display all snippets") {
+				$('.getData').html("Hide snippets");
+			} else {
+				$('.getData').html("Display all snippets");
+			}
+		});
 	});
 
+	$('.searchData').on('click', function(event) {
+		let searchedSnippetNameField = $("#searchSnippetName");
+		let snippetName = searchedSnippetNameField.val();
+		searchedSnippetNameField.val(''); // clear search field
+		let snippet = localStorage.getItem(snippetName);
+		let searchedSnippet = $('.searchedSnippet');
+		searchedSnippet.empty();
+		if (snippet == null) {
+			$('<p></p>').addClass("error")
+			.html("Could not find a snippet with that name. Try searching again.")
+			.appendTo(searchedSnippet);
+		} else {
+			let snippetContainer = $('<div></div>').addClass("snippet");
+			$('<h4></h4>').html(snippetName)
+			.appendTo(snippetContainer);
+			$('<code></code>').html(snippet)
+			.appendTo(snippetContainer);
+			snippetContainer.appendTo(searchedSnippet);
+		}
+		event.preventDefault();
+	});
 	// $('.textField').on('keyup', function(event){
 	// 	let $textFieldValue = $('.textField').val();
 	// 	$('.debug').text($textFieldValue);
